3da6dde415dffa2d00bfeb12b3a32fd7501a47af
Immortal230719 <golub010919@mail.ru>
2019-12-15 21:01:14 +0400

    first step done, without cockie

--- js/app.js
+++ js/app.js
@@ -1,42 +1,9 @@
 "Use Strict";
 
 $(document).ready(function() {
-  // step 1
-
-  $("#startBtn").on("click", function() {
-    $("#welcome").fadeOut(0);
-    $("#stepOne").fadeIn(1000);
-  });
-
-  $("#formStepOne select").on("change", function(e) {
-    switch ($(this).val()) {
-      case "rus": {
-        $("#gerCitySelect").fadeOut(0);
-        $("#usaCitySelect").fadeOut(0);
-        $("#rusCitySelect").fadeIn();
-        break;
-      }
-      case "ger": {
-        $("#rusCitySelect").fadeOut(0);
-        $("#usaCitySelect").fadeOut(0);
-        $("#gerCitySelect").fadeIn();
-        break;
-      }
-      case "usa": {
-        $("#gerCitySelect").fadeOut(0);
-        $("#rusCitySelect").fadeOut(0);
-        $("#usaCitySelect").fadeIn();
-        break;
-      }
-      default:
-        return;
-    }
-    console.log($(this).val());
-  });
-
   // validate forms
 
-  $("#formStepOne").validate({
+  var validator = $("#formStepOne").validate({
     rules: {
       name: {
         required: true,
@@ -61,6 +28,83 @@ $(document).ready(function() {
         min: "You too young for this service",
         max: "Unfortunally, you must be under the Gravestone with that age O_o"
       }
+    },
+    focusCleanup: true
+  });
+
+  // function reset Form
+
+  function resetForm($form) {
+    $form.find("input, select").val("");
+  }
+
+  // first page
+
+  $("#startBtn").on("click", function() {
+    $("#welcome").fadeOut(0);
+    $("#stepOne").fadeIn(1000);
+  });
+
+  // step 1
+
+  // button back
+  $("#backStep1").on("click", function() {
+    $("#stepOne").fadeOut(0);
+    $("#welcome").fadeIn(1000);
+    $("#gerCitySelect").fadeOut(0);
+    $("#usaCitySelect").fadeOut(0);
+    $("#rusCitySelect").fadeOut(0);
+    $("#nextStep1").prop("disabled", true);
+    validator.resetForm();
+    resetForm($("#formStepOne"));
+    $("#formStepOne input").removeClass("error");
+  });
+
+  $("#countrySelect").on("change", function(e) {
+    switch ($(this).val()) {
+      case "rus": {
+        $("#gerCitySelect").fadeOut(0);
+        $("#usaCitySelect").fadeOut(0);
+        $("#rusCitySelect").fadeIn();
+        break;
+      }
+      case "ger": {
+        $("#rusCitySelect").fadeOut(0);
+        $("#usaCitySelect").fadeOut(0);
+        $("#gerCitySelect").fadeIn();
+        break;
+      }
+      case "usa": {
+        $("#gerCitySelect").fadeOut(0);
+        $("#rusCitySelect").fadeOut(0);
+        $("#usaCitySelect").fadeIn();
+        break;
+      }
+      default:
+        return;
     }
+  });
+
+  // button next enabled
+
+  var citySelect = $("#formStepOne select[name='city']");
+
+  for (let index = 0; index < citySelect.length; index++) {
+    $(citySelect[index]).on("change", function() {
+      if ($("#userName").val().length >= 2 && $("#userAge").val() <= 120) {
+        $("#nextStep1").prop("disabled", false);
+      }
+    });
+  }
+
+  // form data variables
+
+  $("#formStepOne").on("submit", function(e) {
+    e.preventDefault();
+    var name = $(this.name).val();
+    var age = $(this.age).val();
+    var country = $(this.country).val();
+    var city = $(this.city).val();
+    console.log(name, age, country, city);
   });
 });
